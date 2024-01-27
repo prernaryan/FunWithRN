680d260c2e7944fed8bd324751b4c9129866324b
prernaryan <prernasharma62333@gmail.com>
2024-01-27 21:19:52 +0530

    'formatted'

--- src/screens/Youtube/YoutubeShorts/index.js
+++ src/screens/Youtube/YoutubeShorts/index.js
@@ -1,4 +1,4 @@
-import {View, Text, FlatList} from 'react-native';
+import {View, FlatList} from 'react-native';
 import React from 'react';
 import {useTheme} from '../../../theme/ThemeProvider';
 import ShortsComp from '@components/Youtubecomp/ShortsComp';
